dcd9099536345b782e7bb9d6c6a7cefaa6c6ea2d
Tom Craig <thetomcraig@PHILO-2.local>
2015-01-19 13:54:37 -0800

    Followed guide more; made gyro.ja values output to elemetns

--- workspace/easy_plan/app/assets/javascripts/accelerometer.js
+++ workspace/easy_plan/app/assets/javascripts/accelerometer.js
@@ -1,36 +1,91 @@
-function accelerometer()
+function start_tracking()
 {
     
-window.addEventListener("orientationchange", function() 
-    {
-        document.getElementById("orientation").innerHTML = "orientation: " + (window.orientation);
-    },
-                        
+    window.addEventListener("orientationchange", function() 
+        {
+            document.getElementById("orientation").innerHTML = "orientation: " + (window.orientation);
+        },
+                            
     false);
+    
+    
+    gyro.frequency = 100;
+    
+    gyro.startTracking(function(o) 
+        {
+            document.getElementById("accelerometer_x").innerHTML = 
+            "accelerometer_x: " + o.x;
+            
+            document.getElementById("accelerometer_y").innerHTML = 
+            "accelerometer_y: " + o.y;
+            
+            document.getElementById("accelerometer_z").innerHTML = 
+            "accelerometer_z: " + o.z;
+    
+             
+            document.getElementById("gyroscope_alpha").innerHTML = 
+            "gyroscope_alpha: " + o.alpha;
+            
+            document.getElementById("gyroscope_beta").innerHTML = 
+            "gyroscope_beta: " + o.beta;
+            
+            document.getElementById("gyroscope_gamma").innerHTML = 
+            "gyroscope_gamma: " + o.gamma;
+        }
+    );
+}
 
+function stop_tracking()
+{
+    gyro.stopTracking();
+}
 
-gyro.frequency = 100;
+function put_values_in_article()
+{
+    
+    document.getElementById("article_title").value = get_date();
+    
+    document.getElementById("article_text").value = 
+        document.getElementById("orientation").innerHTML +
+        "\n" +
+        document.getElementById("accelerometer_x").innerHTML +
+        "\n" +
+        document.getElementById("accelerometer_y").innerHTML +
+        "\n" +
+        document.getElementById("accelerometer_z").innerHTML +
+        "\n" +
+        document.getElementById("accelerometer_x").innerHTML +
+        "\n" +
+        document.getElementById("gyroscope_alpha").innerHTML +
+        "\n" +
+        document.getElementById("gyroscope_beta").innerHTML +
+        "\n" +
+        document.getElementById("gyroscope_gamma").innerHTML ;
+   
+}
 
-gyro.startTracking(function(o) 
-    {
-        document.getElementById("accelerometer_x").innerHTML = 
-        "accelerometer_x: " + o.x;
-        
-        document.getElementById("accelerometer_y").innerHTML = 
-        "accelerometer_y: " + o.y;
-        
-        document.getElementById("accelerometer_z").innerHTML = 
-        "accelerometer_z: " + o.z;
+function get_date() {
 
-         
-        document.getElementById("gyroscope_alpha").innerHTML = 
-        "gyroscope_alpha: " + o.alpha;
-        
-        document.getElementById("gyroscope_beta").innerHTML = 
-        "gyroscope_beta: " + o.beta;
-        
-        document.getElementById("gyroscope_gamma").innerHTML = 
-        "gyroscope_gamma: " + o.gamma;
-    });
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
 }
 
